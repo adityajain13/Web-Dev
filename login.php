49459367f792961e5c9eb24fe86a067f653946ee
Aditya Jain <aditya.a.jain.a.13.a.07.a.1997@gmail.com>
2020-10-04 16:51:49 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,7 @@
 <?php
 include "dbconnect.php";
 
-if(isset($_POST['submit'])) {
-	if($_POST['submit']=="login") { 
-		$username=$_POST['login_username'];
-		$password=$_POST['login_password'];
-		$query = "SELECT * from users where UserName ='$Username' AND Password='$Password'";
-		$result = mysqli_query($con,$query)or die(mysql_error());
-		if(mysqli_num_rows($result) > 0) {
-			$row = mysqli_fetch_assoc($result);
-			$_SESSION['user']=$row['UserName'];
-			header("Location: index.php?login=" . "Successfully Logged In");
-		}
-		else
-			echo "Incorrect username or password";
-	}
-}
+
 
 ?>
 
